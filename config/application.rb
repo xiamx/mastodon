f884c02c274c11e0c174751444c958f546e8291a
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-06-15 21:59:33 -0400

    sentryworker use perform_async

--- config/application.rb
+++ config/application.rb
@@ -138,7 +138,7 @@ module Mastodon
     Raven.configure do |config|
       config.dsn = 'https://a863330c88da4a64b0af64519762b97f:5e4952621ee14cd18ac8ebaadc29ce4d@o407124.ingest.sentry.io/5275722'
       config.async = lambda { |event|
-        SentryWorker.perform_later(event)
+        SentryWorker.perform_async(event)
       }
     end
 
