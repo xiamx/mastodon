fc78366b359893024f0aa71bf0940b773cfe3b2c
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-06-16 23:47:05 -0400

    deploy script is up

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ssh mastodon@m.gretaoto.ca "source \$HOME/.profile && cd live && git pull && bundle && bundle exec rails assets:precompile && bundle exec rails db:migrate"
+ssh root@m.gretaoto.ca "systemctl reload mastodon-web && systemctl restart mastodon-sidekiq && systemctl restart mastodon-streaming"
+
