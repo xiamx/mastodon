8f34481949d04b3f3b527e6acf1b837c4089b846
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-06-22 20:43:51 -0400

    fix migration

--- db/migrate/20200622233838_backfill_cross_site_subscription_created_by.rb
+++ db/migrate/20200622233838_backfill_cross_site_subscription_created_by.rb
@@ -1,7 +1,7 @@
 class BackfillCrossSiteSubscriptionCreatedBy < ActiveRecord::Migration[5.2]
   def up
     CrossSiteSubscription.all.find_each do |sub|
-      if User.find(sub.user_id)
+      if User.find_by(id: sub.user_id)
         sub.created_by_id = sub.user_id
         sub.save!(validate: false)
       end
