373e485bc44bc0bf29bddee910ebc65439812e31
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-06-22 20:47:51 -0400

    fix migration

--- db/migrate/20200623001456_backfill_cross_site_subscription_account.rb
+++ db/migrate/20200623001456_backfill_cross_site_subscription_account.rb
@@ -2,8 +2,10 @@ class BackfillCrossSiteSubscriptionAccount < ActiveRecord::Migration[5.2]
   def up
     CrossSiteSubscription.all.find_each do |sub|
       account = Account.find_by(username: sub.normalized_account_username)
-      sub.account_id = account.id
-      sub.save!
+      if account.present?
+        sub.account_id = account.id
+        sub.save!
+      end
     end
   end
 end
