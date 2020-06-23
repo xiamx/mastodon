55cd8c34a5b87f36b712877e4915457c6f0ba47b
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-06-23 14:12:39 -0400

    fix sidekiq jobs

--- app/lib/cross_site_twitter.rb
+++ app/lib/cross_site_twitter.rb
@@ -57,7 +57,7 @@ class CrossSiteTwitter
   private
 
   def process_tweet!(tweet)
-    cross_site_subscription = CrossSiteSubscription.find_by(site: 'twitter', foreign_user_id: tweet.user.screen_name.downcase)
+    cross_site_subscription = CrossSiteSubscription.find_by(site: 'twitter', foreign_user_id: tweet.user.name.downcase)
     account = create_account_if_not_exist(cross_site_subscription)
     tweet_db_obj = persist_or_find_tweet!(tweet, account)
     publish_tweet!(tweet_db_obj)
