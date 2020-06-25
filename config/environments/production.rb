a8b8bf8c28931d4e49047b78d9c97c537d48be59
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-06-25 09:35:51 -0400

    ignore more error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
     }
     config.processors -= [Raven::Processor::PostData] # Do this to send POST data
     config.processors -= [Raven::Processor::Cookies] # Do this to send cookies by default
-    config.excluded_exceptions += %w[HTTP::TimeoutError HTTP::ConnectionError OpenSSL::SSL::SSLError]
+    config.excluded_exceptions += %w[HTTP::TimeoutError HTTP::ConnectionError OpenSSL::SSL::SSLError Stoplight::Error::RedLight]
   end
 
   config.x.otp_secret = ENV.fetch('OTP_SECRET')
