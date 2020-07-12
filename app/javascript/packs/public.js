762e12a9e0df248565e8e5e0bb4e3abd68e75aac
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-07-12 14:47:27 -0400

    analytics in separate filee

--- app/javascript/packs/public.js
+++ app/javascript/packs/public.js
@@ -263,15 +263,3 @@ loadPolyfills()
   .catch(error => {
     console.error(error);
   });
-
-var _paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u='//analytics.gretaoto.ca/';
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '2']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
