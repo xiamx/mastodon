9a4b03d3e89b66431ea73949cb697081715819aa
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-07-19 12:25:02 -0400

    added cli for cross site subscribes

---
+++ lib/mastodon/cross_site_subscribe_cli.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+require 'set'
+require_relative '../../config/boot'
+require_relative '../../config/environment'
+require_relative 'cli_helper'
+
+module Mastodon
+  class CrossSiteSubscribeCli < Thor
+    include CLIHelper
+
+    def self.exit_on_failure?
+      true
+    end
+
+    option :all, type: :boolean
+    desc 'mute [SITE] [foreign_user_id]', 'mute a cross site subscribed user'
+    def mute(site = nil, foreign_user_id = nil)
+      if options[:all]
+        where_clause = nil
+      elsif !site.nil? && !foreign_user_id.nil?
+        where_clause = { site: site, foreign_user_id: foreign_user_id }
+      else
+        say('No cross_site_subscription(s) given', :red)
+        exit(1)
+      end
+
+      CrossSiteSubscription.where(where_clause).find_each do |sub|
+        account = sub.account
+        next if account.nil?
+
+        user = account.user
+        next if user.nil?
+
+        user.settings['default_privacy'] = :unlisted
+        user.save!
+
+        say("Muted #{sub.site} #{sub.foreign_user_id}", :green)
+
+        account.statuses.each do |status|
+          status.visibility = :unlisted
+          status.save!
+        end
+        say("All #{sub.site} #{sub.foreign_user_id} statuses are now unlisted", :green)
+
+      end
+      say('OK', :green)
+    end
+  end
+end
