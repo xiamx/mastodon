3185fbb6c2efa8bc4838f4b6bb57a67b81903eb6
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-07-23 10:47:13 -0400

    update deploy script to work with forced push

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git push
-ssh kimsufi 'echo "export RAILS_ENV=production && sleep 1 && source \$HOME/.bashrc && cd live && git pull && /home/mastodon/.rbenv/shims/bundle && /home/mastodon/.rbenv/shims/bundle exec rails assets:precompile && /home/mastodon/.rbenv/shims/bundle exec rails db:migrate" | lxc exec gretaoto-mastodon -- su --login mastodon'
+ssh kimsufi 'echo "export RAILS_ENV=production && sleep 1 && source \$HOME/.bashrc && cd live && git fetch && git reset origin/master --soft && /home/mastodon/.rbenv/shims/bundle && /home/mastodon/.rbenv/shims/bundle exec rails assets:precompile && /home/mastodon/.rbenv/shims/bundle exec rails db:migrate" | lxc exec gretaoto-mastodon -- su --login mastodon'
 ssh kimsufi 'lxc exec gretaoto-mastodon -- bash -c "systemctl reload mastodon-web && systemctl restart mastodon-sidekiq && systemctl restart mastodon-streaming"'
 
