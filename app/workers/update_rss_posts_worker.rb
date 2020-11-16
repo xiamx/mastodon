9d5a3c69dd02e678f24f32e1b98c8ef47e35fcc0
Meng Xuan Xia <mengxuan.xia@outlook.com>
2020-11-15 20:57:30 -0500

    don't retry update rss posts

--- app/workers/update_rss_posts_worker.rb
+++ app/workers/update_rss_posts_worker.rb
@@ -2,6 +2,7 @@
 
 class UpdateRssPostsWorker
   include Sidekiq::Worker
+  sidekiq_options retry: 0
 
   def perform(site, subscription_id)
     CrossSiteGeneric.new(site).update!(CrossSiteSubscription.find(subscription_id))
